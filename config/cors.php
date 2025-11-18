e3eedb41311a40df95929970a1ac68d63b9abc2b
HAWKCAN <farizrahman335577@gmail.com>
2025-11-18 20:47:17 +0700

    test

--- config/cors.php
+++ config/cors.php
@@ -6,11 +6,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'https://skedul.webinyuk.store',
-    ],
+'allowed_origins' => [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'https://skedul.webinyuk.store',
+],
+
 
     'allowed_origins_patterns' => [],
 
